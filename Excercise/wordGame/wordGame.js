bd03bc61da19814439c20bc22c79240588e45532
Hamidah <hamidaha@l5lab.com>
2018-08-13 10:57:20 +0100

    used logical statements to control the program flow

--- Excercise/wordGame/wordGame.js
+++ Excercise/wordGame/wordGame.js
@@ -1,4 +1,12 @@
 var guessedWords = [];
-for (var guess = 0; guess <= 2; guess++){
+for (var guess = 0; guess <= 2; guess++) {
     var guessedWord = prompt("enter your word");
+    if(guessedWord === "secret") {
+        alert("correct");
+    }
+    else {
+        alert("wrong");
+    }
+    guessedWords.push(guessedWord);
 }
+console.log(guessedWords);
